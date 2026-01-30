71df7389961f7555928daaa624c767ed7ed05395
Grojman <gomisromanjorge@gmail.com>
2026-01-30 21:17:15 +0100

    player

---
+++ Assets/Scripts/Mask.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class Mask : MonoBehaviour
+{
+    public float TimeMask = 5f;
+    public float CooldownMask = 5f;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    protected virtual void OnGet(GameObject player)
+    {
+        
+    }
+
+    protected virtual void OnClose(GameObject player)
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,54 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player : MonoBehaviour
+{
+    public Vector2 AttackDirection;
+    public Rigidbody2D rg;
+    public PlayerInput playerInput;
+    public Vector2 input;
+    public Vector2 movement;
+    public const int DEFAULT_HIT_POINTS = 3;
+    public float Speed = 5f;
+    public float Damage = 5f;
+    public float JumpForce = 5f;
+    public int HitPoints;
+    public Transform SpawnPoint;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rg = GetComponent<Rigidbody2D>();
+        playerInput = GetComponent<PlayerInput>();
+        transform.position = SpawnPoint.position;
+        HitPoints = DEFAULT_HIT_POINTS;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        input = playerInput.actions["Move"].ReadValue<Vector2>();
+        AttackDirection = playerInput.actions["Aim"].ReadValue<Vector2>();
+        movement = new Vector2(Time.deltaTime * Speed * input.x, 0);
+    }
+
+    void FixedUpdate()
+    {
+        rg.AddForce(movement);
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if(context.performed)
+        {
+            rg.AddForce(Vector2.up * JumpForce);
+        }
+    }
+
+    public void Attack(InputAction.CallbackContext context)
+    {
+        if (context.performed)
+        {
+            //Si tiene una mascara que cambie su ataque, pues eso
+        }
+    }
+}
